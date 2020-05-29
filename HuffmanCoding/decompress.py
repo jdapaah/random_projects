397a257a20bc3d971169e9b4c3824075e0ffa4b6
Jeremy D <61062668+jdapaah@users.noreply.github.com>
2020-05-29 15:06:51 -0400

    Add files via upload

--- HuffmanCoding/decompress.py
+++ HuffmanCoding/decompress.py
@@ -2,59 +2,41 @@
 for recovering the original file from compressed versions using the 'adjacent' tree"""
 from pickle import load
 from sys import argv
+# from HuffmanCoding.compress import BinaryTree
 from compress import BinaryTree
 
 
-# def decompress(fname, peach):
-# 	with open(fname, 'rb') as read, open('Decoded_' + fname[8:], 'w') as write:
-# 		bitstring = ''
-# 		node = peach
-# 		ind = 0
-# 		for line in read:
-# 			for byte in line:  # read 8 bits
-# 				one = bin(int(byte))[2:]  # bin() looses some front-padding 0s
-# 				one = '0' * [0, 7, 6, 5, 4, 3, 2, 1][len(one) % 8] + one  # add back 0s lost by bin()
-# 				bitstring += one
-# 				node
-# 				if node.char != '*':
-# 					write.write(node.char)
-
-
-
-# 		del line, byte, one, read
-# 		code = ''
-# 		for bit in bitstring:
-# 			code += bit
-# 			char, code = letter(code, peach)  # get the character to print and the code for the next letter
-
-
-
 def decompress(fname, peach):
-	with open(fname, 'rb') as read, open('Decoded_' + fname[8:], 'w') as write:
+	max_len = 0
+	with open("Huffman_" + fname, 'rb') as read, open('Decoded_' + fname, 'w') as write:
 		bitstring = ''
-		for line in read:
+		for line in read:  # will stop at but include newlines
 			for byte in line:  # read 8 bits
-				one = bin(int(byte))[2:]  # bin() looses some front-padding 0s
-				one = '0' * [0, 7, 6, 5, 4, 3, 2, 1][len(one) % 8] + one  # add back 0s lost by bin()
-				bitstring += one
-		del line, byte, one, read
-		code = ''
-		for bit in bitstring:
-			code += bit
-			char, code = letter(code, peach)  # get the character to print and the code for the next letter
-			write.write(char)
+				bits = bin(int(byte))[2:]  # bin() looses some front-padding 0 bits
+				bits = '0' * [0, 7, 6, 5, 4, 3, 2, 1][len(bits) % 8] + bits  # add back 0s lost by bin()
+				bitstring += bits
+				max_len = max(max_len, len(bitstring))
+				chars, bitstring = letters(bitstring, peach)  # get as many as letters as you can
+				write.write(chars)
+	print(max_len)
 
 
-def letter(path, peach):
-	for bit in path:  # trace down the tree
-		peach = peach.left if bit == '0' else peach.right  # import BinaryTree
-	if peach.char == '*':
-		return '', path  # if the code does not map to a leaf, this will write nothing
-	return peach.char, ''  # if the code maps to a leaf, clear the code for the next character
+def letters(path, peach):
+	collected = ''
+	last_starting_point = 0
+	node = peach
+	for index, bit in enumerate(path):  # trace down the tree
+		node = node.left if bit == '0' else node.right  # import BinaryTree
+		if node.char != '*':
+			collected += node.char
+			last_starting_point = index+1
+			node = peach
+	return collected, path[last_starting_point:]
 
 
 if __name__ == '__main__':
-	filename = argv[1]
-	with open('PickledTree_'+filename[8:], 'rb') as rick:
+	filename = argv[1][8:]
+	with open('PickledTree_' + filename, 'rb') as rick:
 		tree = load(rick)
+	print(tree)
 	decompress(filename, tree)
